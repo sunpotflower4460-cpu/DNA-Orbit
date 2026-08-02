b18bc796d78394e5ba2289ebd79e2eece1242460
sunpotflower4460-cpu <sunpotflower4460@gmail.com>
2026-08-02 10:11:29 +0900

    feat: expose bass, character and phase controls in detail UI

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -18,11 +18,12 @@ public:
 private:
     using SliderAttachment = juce::AudioProcessorValueTreeState::SliderAttachment;
     using ButtonAttachment = juce::AudioProcessorValueTreeState::ButtonAttachment;
-    using ComboAttachment  = juce::AudioProcessorValueTreeState::ComboBoxAttachment;
+    using ComboAttachment = juce::AudioProcessorValueTreeState::ComboBoxAttachment;
 
     struct Knob
     {
-        juce::Slider slider { juce::Slider::RotaryHorizontalVerticalDrag, juce::Slider::TextBoxBelow };
+        juce::Slider slider { juce::Slider::RotaryHorizontalVerticalDrag,
+                              juce::Slider::TextBoxBelow };
         juce::Label nameLabel;
         juce::Label hintLabel;
         std::unique_ptr<SliderAttachment> attachment;
@@ -30,6 +31,10 @@ private:
 
     void setUpKnob (Knob&, const juce::String& paramID, const juce::String& name,
                     const juce::String& hint, const juce::String& tooltip);
+    void setUpChoice (juce::ComboBox&, juce::Label&, const juce::StringArray& items,
+                      const juce::String& label, const juce::String& tooltip,
+                      std::unique_ptr<ComboAttachment>& attachment,
+                      const juce::String& paramID);
     void layOutKnobRow (juce::Rectangle<int> row, const std::vector<Knob*>& knobs);
     void showPage (int page);
     void applyPreset (int presetIndex);
@@ -39,7 +44,6 @@ private:
     static juce::Font japaneseFont (float height, bool bold = false);
 
     DNAOrbitAudioProcessor& processorRef;
-
     dnaorbit::ui::DnaLookAndFeel lookAndFeel;
     dnaorbit::ui::HelixView3D helixView;
     juce::TooltipWindow tooltipWindow { this, 600 };
@@ -55,10 +59,11 @@ private:
 
     Knob rateKnob, radiusKnob, depthKnob, mixKnob;
     Knob symmetryKnob, twistKnob, coreKnob, outputKnob, stereoPreserveKnob;
+    Knob bassAnchorKnob, startPhaseKnob;
 
     juce::ToggleButton syncButton;
-    juce::ComboBox divisionBox;
-    juce::Label divisionLabel;
+    juce::ComboBox divisionBox, characterBox, phaseModeBox, directionBox;
+    juce::Label divisionLabel, characterLabel, phaseModeLabel, directionLabel;
     juce::ToggleButton autoGainButton;
     juce::ToggleButton softBypassButton;
     juce::ToggleButton nullCoreButton;
@@ -68,9 +73,14 @@ private:
     std::unique_ptr<ButtonAttachment> softBypassAttachment;
     std::unique_ptr<ButtonAttachment> nullCoreAttachment;
     std::unique_ptr<ComboAttachment> divisionAttachment;
+    std::unique_ptr<ComboAttachment> characterAttachment;
+    std::unique_ptr<ComboAttachment> phaseModeAttachment;
+    std::unique_ptr<ComboAttachment> directionAttachment;
 
     juce::Label readoutLabel, statusLabel, warningLabel;
     std::unique_ptr<juce::ParameterAttachment> nullCoreWatcher;
+
+    static constexpr int controlPanelHeight = 184;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (DNAOrbitAudioProcessorEditor)
 };
