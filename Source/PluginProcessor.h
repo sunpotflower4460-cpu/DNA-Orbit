eb7f3a240d4e6435e2a94a9b2b30b4d9c8766c7c
sunpotflower4460-cpu <sunpotflower4460@gmail.com>
2026-08-02 10:08:16 +0900

    feat: pass musical DSP and transport state to engine

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -12,7 +12,6 @@ public:
 
     void prepareToPlay (double sampleRate, int samplesPerBlock) override;
     void releaseResources() override;
-
     bool isBusesLayoutSupported (const BusesLayout& layouts) const override;
 
     using AudioProcessor::processBlock;
@@ -22,13 +21,12 @@ public:
 
     juce::AudioProcessorEditor* createEditor() override;
     bool hasEditor() const override { return true; }
-
     const juce::String getName() const override { return JucePlugin_Name; }
 
     bool acceptsMidi() const override { return false; }
     bool producesMidi() const override { return false; }
     bool isMidiEffect() const override { return false; }
-    double getTailLengthSeconds() const override { return 0.05; }
+    double getTailLengthSeconds() const override { return 0.06; }
 
     int getNumPrograms() override { return 1; }
     int getCurrentProgram() override { return 0; }
@@ -40,7 +38,6 @@ public:
     void setStateInformation (const void* data, int sizeInBytes) override;
 
     juce::AudioProcessorValueTreeState apvts;
-
     dnaorbit::dsp::HelixEngine& getEngine() noexcept { return engine; }
     int getLoadedSchemaVersion() const noexcept { return loadedSchemaVersion; }
 
@@ -48,25 +45,30 @@ private:
     dnaorbit::dsp::HelixEngine engine;
     juce::AudioBuffer<float> bypassScratchBuffer;
 
-    std::atomic<float>* rateHzParam     = nullptr;
-    std::atomic<float>* syncParam       = nullptr;
-    std::atomic<float>* divisionParam   = nullptr;
-    std::atomic<float>* radiusParam     = nullptr;
-    std::atomic<float>* depthParam      = nullptr;
-    std::atomic<float>* symmetryParam   = nullptr;
-    std::atomic<float>* twistParam      = nullptr;
-    std::atomic<float>* coreParam       = nullptr;
-    std::atomic<float>* nullCoreParam   = nullptr;
-    std::atomic<float>* mixParam        = nullptr;
-    std::atomic<float>* outputParam     = nullptr;
-    std::atomic<float>* autoGainParam   = nullptr;
+    std::atomic<float>* rateHzParam = nullptr;
+    std::atomic<float>* syncParam = nullptr;
+    std::atomic<float>* divisionParam = nullptr;
+    std::atomic<float>* radiusParam = nullptr;
+    std::atomic<float>* depthParam = nullptr;
+    std::atomic<float>* symmetryParam = nullptr;
+    std::atomic<float>* twistParam = nullptr;
+    std::atomic<float>* coreParam = nullptr;
+    std::atomic<float>* nullCoreParam = nullptr;
+    std::atomic<float>* mixParam = nullptr;
+    std::atomic<float>* outputParam = nullptr;
+    std::atomic<float>* autoGainParam = nullptr;
     std::atomic<float>* stereoPreserveParam = nullptr;
     std::atomic<float>* softBypassParam = nullptr;
+    std::atomic<float>* bassAnchorParam = nullptr;
+    std::atomic<float>* characterParam = nullptr;
+    std::atomic<float>* phaseModeParam = nullptr;
+    std::atomic<float>* startPhaseParam = nullptr;
+    std::atomic<float>* directionParam = nullptr;
 
     int loadedSchemaVersion = dnaorbit::params::currentStateSchemaVersion;
+    bool previousTransportPlaying = false;
 
-    float resolveRateHz() const noexcept;
-    dnaorbit::dsp::HelixEngine::Parameters currentParameterSnapshot() const noexcept;
+    dnaorbit::dsp::HelixEngine::Parameters currentParameterSnapshot() noexcept;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (DNAOrbitAudioProcessor)
 };
